85aec15f0f3a096873e2480fc22c9403fcec8ffa
react-timmy <154727321+react-timmy@users.noreply.github.com>
2026-09-17 07:57:59 -0400

    chore: remove junk, IDE files, and build artifacts from tracking

--- .next/dev/static/chunks/devtimmy-portfolio_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1q_ndco._.css","static/chunks/devtimmy-portfolio_0-p-577._.js","static/chunks/0cce_next_dist_1d5nxkg._.js","static/chunks/devtimmy-portfolio_app_0pni5uc._.js"],
-    source: "entry"
-});
